bc1caafc9081298660dfe1ec45a4e6e5fd2bd13a
Evan <justgamerimportant@gmail.com>
2019-03-16 14:43:21 -0700

    Update index.js

--- Shapes Practice/Star/index.js
+++ Shapes Practice/Star/index.js
@@ -1,24 +1,24 @@
 var star = function(x, y, size, strokeColor = color(0)) {
   noStroke();
-  triangle(x + size/2, y + size/4, x - size/2, y + size/4, x, y - size/2);
-  triangle(x + size/2, y - size/4, x - size/2, y - size/4, x, y + size/2);
+  triangle(x + size / 2, y + size / 4, x - size / 2, y + size / 4, x, y - size / 2);
+  triangle(x + size / 2, y - size / 4, x - size / 2, y - size / 4, x, y + size / 2);
 
   if(strokeColor) {
     stroke(strokeColor);
-    line(x + size * 0.332, y, x+size/2, y+size/4);
-    line(x + size * 0.332, y, x+size/2, y-size/4);
-    line(x - size * 0.332, y, x-size/2, y+size/4);
-    line(x - size * 0.332, y, x-size/2, y-size/4);
+    line(x + size * 0.332, y, x+size / 2, y + size / 4);
+    line(x + size * 0.332, y, x+size / 2, y - size / 4);
+    line(x - size * 0.332, y, x-size / 2, y + size / 4);
+    line(x - size * 0.332, y, x-size / 2, y - size / 4);
 
-    line(x + size * 0.164, y + size/4, x, y + size/2);
-    line(x - size * 0.164, y + size/4, x, y + size/2);
-    line(x + size * 0.164, y - size/4, x, y - size/2);
-    line(x - size * 0.164, y - size/4, x, y - size/2);
+    line(x + size * 0.164, y + size / 4, x, y + size / 2);
+    line(x - size * 0.164, y + size / 4, x, y + size / 2);
+    line(x + size * 0.164, y - size / 4, x, y - size / 2);
+    line(x - size * 0.164, y - size / 4, x, y - size / 2);
 
-    line(x + size * 0.164, y + size/4, x+size/2, y + size/4);
-    line(x - size * 0.164, y + size/4, x-size/2, y + size/4);
-    line(x + size * 0.164, y - size/4, x+size/2, y - size/4);
-    line(x - size * 0.164, y - size/4, x-size/2, y - size/4);
+    line(x + size * 0.164, y + size / 4, x + size / 2, y + size / 4);
+    line(x - size * 0.164, y + size / 4, x - size / 2, y + size / 4);
+    line(x + size * 0.164, y - size / 4, x + size / 2, y - size / 4);
+    line(x - size * 0.164, y - size / 4, x - size / 2, y - size / 4);
   }
 
 };
