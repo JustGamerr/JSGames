59a4b58a13351cab086f388a562c0ff7973305d1
Evan <justgamerimportant@gmail.com>
2019-03-16 14:38:55 -0700

    Update index.js

--- Shapes Practice/Lightning/index.js
+++ Shapes Practice/Lightning/index.js
@@ -1,19 +1,20 @@
 var lightning = function(x, y, width, height, strokeColor= color(0)) {
   noStroke();
-  quad(x - width/2-3, y + height/20, x+3, y + height/20, x + width/2+3, y - height/20, x-3, y - height/20);
-  triangle(x - width/2, y + height/20, x, y - height/20, x + width/2, y - 50);
-  triangle(x + width/2, y - height/20, x, y + height/20, x - width/2, y + 50);
+  quad(x - width / 2 - 3, y + height / 20, x + 3, y + height / 20, x + width / 2 + 3, y - height / 20, x - 3, y - height / 20);
+  triangle(x - width / 2, y + height / 20, x, y - height / 20, x + width / 2, y - 50);
+  triangle(x + width / 2, y - height / 20, x, y + height / 20, x - width / 2, y + 50);
 
-  if(strokeColor){
+  if(strokeColor) {
     stroke(strokeColor);
-    line(x-width/2, y+height/20, x, y+height/20);
-    line(x, y+height/20, x-width/2, y+height/2);
-    line(x - width/2, y+height/2, x+width/2, y-height/20);
-    line(x+width/2, y-height/20, x, y-height/20);
-    line(x, y-height/20, x+width/2, y-height/2);
-    line(x+width/2, y-height/2, x-width/2, y+height/20)
+    line(x - width / 2, y + height / 20, x, y + height / 20);
+    line(x, y + height / 20, x - width / 2, y + height / 2);
+    line(x - width / 2, y + height / 2, x + width / 2, y - height / 20);
+    line(x + width / 2, y - height / 20, x, y - height / 20);
+    line(x, y - height / 20, x + width / 2, y - height / 2);
+    line(x + width / 2, y - height / 2, x - width/2, y + height / 20)
   }
 }
 
 lightning(10, 100, 50, 200);
+
 lightning(200, 300, 200, 50);
