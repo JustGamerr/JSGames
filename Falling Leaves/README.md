84883b6a7c69a4f5737896edc7d046d05a3b2b07
Evan <justgamerimportant@gmail.com>
2019-03-16 14:51:20 -0700

    Falling Leaves Project Setup

---
+++ Falling Leaves/README.md
@@ -0,0 +1,2 @@
+# Falling Leaves
+A leaf falling simulator with pile up mechanics built using ProcessingJS
---
+++ Falling Leaves/index.js
@@ -0,0 +1,38 @@
+var leaves = [];
+var bottom=500;
+var maxSpeed = 10;
+var pileUp=.1;
+var intensity=2;
+var treeImg = loadImage("tree.jpg")
+var leafSize = 25;
+noStroke();
+var setup = function(){
+  leaves=[];
+  while(leaves.length<1000){
+
+    leaves.push({x:random(0,500), y:random(0,-20000/intensity), width:random(leafSize*0.4,leafSize), height:random(leafSize*0.2,leafSize), speed:random(maxSpeed*0.25,maxSpeed), xSpd:random(-4,4), col: color(random(100,255),random(100,255),0)});
+  }
+};
+
+var draw = function(){
+  image(treeImg,0,0,500,500)
+  leaves.forEach(function(leaf){
+    leaf.y += leaf.speed;
+    leaf.x += leaf.xSpd;
+    fill(leaf.col);
+    ellipse(leaf.x,leaf.y,leaf.width,leaf.height);
+    if(leaf.y>bottom&& leaf.speed>0){
+      leaf.speed=0;
+      leaf.xSpd=0;
+      leaf.y=bottom;
+      bottom-=pileUp;
+    }
+    if(leaf.x>500 || leaf.x<0){
+      leaf.xSpd *=-1;
+    }
+  });
+  if(bottom<500.1-leaves.length*pileUp){
+    bottom=500;
+    setup();
+  }
+};
