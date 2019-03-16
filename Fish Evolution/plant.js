a5cd8e289c8f40e9a88c85ccaa01b24b04de4f54
Evan <justgamerimportant@gmail.com>
2019-03-16 11:43:19 -0700

    Fish Evo - Update 2
    "Plant"/food setup and creation class

---
+++ Fish Evolution/plant.js
@@ -0,0 +1,17 @@
+class Plant {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.food = 5;
+        this.size = 10;
+    }
+
+    drawSelf() {
+
+    }
+
+    update() {
+        this.drawSelf();
+    }
+}
