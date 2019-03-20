acd6a5957e501cb6ec83aa16894f321a3de6e424
Evan <justgamerimportant@gmail.com>
2019-03-20 12:45:45 -0700

    Vector squares practice update

--- Vectors Practice/Squares/index.js
+++ Vectors Practice/Squares/index.js
@@ -1,6 +1,10 @@
 var block = {x: 0, y: 50, w: 50, h: 50, xspeed: random(3,20), yspeed: random(5,15), hue: 0}
 var blocka = {x: 50, y: 450, w: 50, h: 50, xspeed: random(1,6), yspeed: random(1,6), hue: 0}
 
+var setup = function() {
+  createCanvas(500, 500);
+}
+
 var draw = function() {
   noStroke()
   colorMode(HSB, 100);
