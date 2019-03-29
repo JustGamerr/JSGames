5325d9bc66e6a7726d974da8afded5faab2cb644
Evan <justgamerimportant@gmail.com>
2019-03-28 18:02:03 -0700

    Other Projects 7 Upload

---
+++ Other/JS7/index8.js
@@ -0,0 +1,31 @@
+var points = []
+
+function setup() {
+  createCanvas(500, 500)
+  noStroke()
+  colorMode(HSB, 100)
+  let radius = 100;
+  let hue = 0;
+  for(let angle = 0; angle < 2*PI; angle += PI/6) {
+      let x = radius * cos(angle);
+      let y = radius * sin(angle);
+
+      x += width/2;
+      y += width/2;
+      hue = angle / (2*PI) * 100;
+      points.push(new Point(x, y, hue, angle));
+  }
+}
+function draw() {
+  fill(0,0,0, 40);
+  if(frameCount % 8 === 0) {
+      rect(0, 0, width, height);
+  }
+
+  for(let point of points) {
+      radius = 70 + 20 * sin(this.angle/6)
+      this.size = 20 * sin(this.angle/3);
+      point.update();
+  }
+
+}
