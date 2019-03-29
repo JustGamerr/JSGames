772408ccfab3067271c1aaa93aa313e9b8038bd6
Evan <justgamerimportant@gmail.com>
2019-03-28 18:01:37 -0700

    Other Projects 4 Upload

---
+++ Other/JS4/points7.js
@@ -0,0 +1,26 @@
+class Point {
+  constructor(centerx, centery, hue, startingAngle, speed, radius) {
+    this.cx = centerx;
+    this.cy = centery;
+    this.hue = hue;
+    this.angle = startingAngle || 0;
+    this.speed = speed || 1;
+    this.radius = radius || 70;
+  }
+  move() {
+    this.angle += this.speed * PI/60;
+    //this.radius = 70 * sin(this.angle);
+  }
+  draw() {
+    let r = this.radius;
+    let x = r * cos(this.angle);
+    let y = r * sin(this.angle);
+
+    fill(this.hue, 100, 100);
+    ellipse(this.cx + x, this.cy + y, 6, 6);
+  }
+  update() {
+    this.move();
+    this.draw();
+  }
+}
