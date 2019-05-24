b03aa4e2552eb50c8cf32a41c4bbc4899cbb7748
Evan <justgamerimportant@gmail.com>
2019-05-24 13:00:12 -0700

    Mouse input checking

--- Cosmic Adventure/Game/keyInput.js
+++ Cosmic Adventure/Game/keyInput.js
@@ -1,13 +1,23 @@
-var register = {}
+var register = {};
 
 function keyPressed()
 {
-  register[keyCode] = true
+  register[keyCode] = true;
 }
 
 function keyReleased()
 {
-  register[keyCode] = false
+  register[keyCode] = false;
 }
 
-window.addEventListener('contextmenu', event => event.preventDefault())
+function mousePressed()
+{
+  register[mouseButton] = true;
+}
+
+function mouseReleased()
+{
+  register[mouseButton] = false;
+}
+
+window.addEventListener('contextmenu', event => event.preventDefault());
