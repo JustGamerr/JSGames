3902eff2f6bff390a39c38c9701738948fdc63b9
Evan <justgamerimportant@gmail.com>
2019-05-24 13:11:04 -0700

    Text movement

--- Cosmic Adventure/Game/menu.js
+++ Cosmic Adventure/Game/menu.js
@@ -15,7 +15,7 @@ class Menu
     fill(this.textColor);
     textAlign(CENTER);
     textSize(56);
-    text(this.text, 250, 75);
+    text(this.text, 250, 100);
     for(var button of this.pages[this.page])
     {
       button.draw();
