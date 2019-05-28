35dd810b67eabf19a52f34ca96ccbd44d499fbe7
Evan <justgamerimportant@gmail.com>
2019-05-28 13:59:54 -0700

    Image location fix

--- Cosmic Adventure/Game/images.js
+++ Cosmic Adventure/Game/images.js
@@ -10,15 +10,15 @@ var p2MovingImg;
 
 function preload()
 {
-  bkgImage = loadImage("http://cosmicadventure.x10.mx/game/resources/bkgImage.jpg");
+  bkgImage = loadImage("http://cosmicadventure.xyz/game/resources/bkgImage.jpg");
 
-  p1Img = loadImage("http://cosmicadventure.x10.mx/game/resources/p1full.gif");
-  p1IdleImg = loadImage("http://cosmicadventure.x10.mx/game/resources/p1idle.png");
-  p1FlameImg = loadImage("http://cosmicadventure.x10.mx/game/resources/p1fullflame.png");
-  p1MovingImg = loadImage("http://cosmicadventure.x10.mx/game/resources/p1moving.gif");
+  p1Img = loadImage("http://cosmicadventure.xyz/game/resources/p1full.gif");
+  p1IdleImg = loadImage("http://cosmicadventure.xyz/game/resources/p1idle.png");
+  p1FlameImg = loadImage("http://cosmicadventure.xyz/game/resources/p1fullflame.png");
+  p1MovingImg = loadImage("http://cosmicadventure.xyz/game/resources/p1moving.gif");
 
-  p2Img = loadImage("http://cosmicadventure.x10.mx/game/resources/p2full.gif");
-  p2IdleImg = loadImage("http://cosmicadventure.x10.mx/game/resources/p2idle.png");
-  p2FlameImg = loadImage("http://cosmicadventure.x10.mx/game/resources/p2fullflame.png");
-  p2MovingImg = loadImage("http://cosmicadventure.x10.mx/game/resources/p2moving.gif");
+  p2Img = loadImage("http://cosmicadventure.xyz/game/resources/p2full.gif");
+  p2IdleImg = loadImage("http://cosmicadventure.xyz/game/resources/p2idle.png");
+  p2FlameImg = loadImage("http://cosmicadventure.xyz/game/resources/p2fullflame.png");
+  p2MovingImg = loadImage("http://cosmicadventure.xyz/game/resources/p2moving.gif");
 }
