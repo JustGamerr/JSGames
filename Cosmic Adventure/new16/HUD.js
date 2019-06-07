fbd92d500938150d98f6ea9613b731f1fcbbbd10
Evan <justgamerimportant@gmail.com>
2019-06-07 10:42:53 -0700

    Fixes

--- Cosmic Adventure/new16/HUD.js
+++ Cosmic Adventure/new16/HUD.js
@@ -13,7 +13,7 @@ function drawHUD(canvas, p)
     //canvas.text("Health: " + p.health, 300, 480);
   } else {
     canvas.text("Coins: " + p.coins, 30, 480);
-    canvas.text("Speed: " + Math.sqrt(p.vx * p.vx + p.vy * p.vy).toFixed(1), 140, 480);
+    canvas.text("Speed: " + Math.sqrt(p.vx * p.vx + p.vy * p.vy).toFixed(1), 155, 480);
     canvas.text("Health: " + p.health, 320, 480);
   }
 }
